3866f46e03120aaa58ca11992d70b2a19f06dde2
sarandevs <31305086+sarandevs@users.noreply.github.com>
2017-09-11 14:46:35 +0530

    Add files via upload

---
+++ 3@prgm51.cpp
@@ -0,0 +1,18 @@
+	int n, nu, num=0, rem;
+	cout<<"Enter any positive number : ";
+	cin>>n;
+	nu=n;
+	while(nu!=0)
+	{
+		rem=nu%10;
+		num=num + rem*rem*rem;
+		nu=nu/10;
+	}
+	if(num==n)
+	{
+		cout<<"Armstrong Number";
+	}
+	else
+	{
+		cout<<"Not Armstrong Number";
+	}
